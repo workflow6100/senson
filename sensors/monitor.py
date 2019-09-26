f97c566f36d3f081f3e8a189cc75fa5ce7f7a3f7
harsh6100 <52690817+harsh6100@users.noreply.github.com>
2019-09-26 13:27:00 +0530

    Update monitor.py

--- sensors/monitor.py
+++ sensors/monitor.py
@@ -14,11 +14,41 @@ class HelloSensor(Sensor):
 
     def run(self):
         while not self._stop:
-            self._logger.debug('HelloSensor dispatching trigger...')
-            count = self.sensor_service.get_value('hello_st2.count') or 0
-            payload = {'greeting': 'Yo, StackStorm!', 'count': int(count) + 1}
-            self.sensor_service.dispatch(trigger='hello_st2.event1', payload=payload)
-            self.sensor_service.set_value('hello_st2.count', payload['count'])
+            SUBSCRIPTION_ID = '2f50f202-0a84-4c8c-a929-fcc5a3174590'
+            GROUP_NAME = 'OmkarVmPlzDoNotRemove'
+            LOCATION = 'West US'
+            VM_NAME = 'OmkarVmPlzDoNotRemoveThis'
+            resource_id = (
+              "subscriptions/{}/"
+              "resourceGroups/{}/"
+              "providers/Microsoft.Compute/virtualMachines/{}"
+               ).format(SUBSCRIPTION_ID, GROUP_NAME, VM_NAME)
+            credentials = get_credentials()
+            client = MonitorManagementClient(
+                 credentials,
+                 SUBSCRIPTION_ID
+                 )        
+            m=[]
+            today = datetime.datetime.now()-timedelta(hours=6)
+            yesterday = today - datetime.timedelta(minutes=5)
+            metrics_data = client.metrics.list(
+                           resource_id,
+                          timespan="{}/{}".format(yesterday, today),
+                          interval='PT1M',
+                          metricnames='Percentage CPU',
+                          aggregation='Maximum'
+                          )
+            for item in metrics_data.value:
+              for timeserie in item.timeseries:
+                for data in timeserie.data:
+                   x=data.maximum
+                   m.append(x)
+            sum=0
+            for m1 in m:
+              sum=sum+m1
+            avg=sum/5              
+            payload = {'average': avg}
+            self.sensor_service.dispatch(trigger='hello_st2.event1', payload=payload)         
             eventlet.sleep(60)
 
     def cleanup(self):
@@ -33,3 +63,10 @@ class HelloSensor(Sensor):
 
     def remove_trigger(self, trigger):
         pass
+    
+    def get_credentials():
+        credentials = ServicePrincipalCredentials(
+                    client_id = '94c1d9ea-ffd1-4340-8f19-d3ad284805d8',
+                    secret = 'uJUcbJbJjN3Y3TXYRR*6lUVuV/Z0Av@]',
+                    tenant = 'd5656af4-b7b3-45b9-9346-fb0547921fb7'
+                    )
